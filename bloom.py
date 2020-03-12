4bc13d01ffcf5e6cfc77287109a338b87c2ae509
dpetzoldt <Dirk.Petzoldt@gmail.com>
2020-03-12 14:11:30 +0100

    Tests pass. But capacities as powers of 2 wasteful.

--- bloom.py
+++ bloom.py
@@ -32,18 +32,18 @@ def inspect_bytes(btes):
     print(f'Bits: {[bin(b) for b in btes]}')
 
 
-def bloom_insertions(m, n):
-    """m -- bits, n -- inserts"""
+def bloom_inserts(m, n):
+    """ Create BloomFilter with good number of has functions for m bits and n insertions """
     k = math.ceil(math.log(2) * m / n)
     print(f"bits={m}, inserts={n} --> hashes={k}")
     return BloomFilter(m, k)
 
 
-def bloom_false_positives(epsilon, n):
-    """epsilon -- accepted false positive rate, n -- inserts"""
-    m = (math.log(epsilon) * n) / (math.log(2) ** 2)
+def bloom_fp(epsilon, n):
+    """ Create BloomFilter with false positive rate of epsilon for n inserts """
+    m = -int(math.ceil(math.log(epsilon) * n) / (math.log(2) ** 2))
     print(f"epsilon={epsilon}, inserts={n} --> bits={m}")
-    return bloom_insertions(next_power_of_2(m), n)
+    return bloom_inserts(next_power_of_2(m), n)
 
 
 class Hasher:
@@ -52,14 +52,15 @@ class Hasher:
     size = 512  # number of bits
 
     def __init__(self, k=8, n=64):
-        """Will hash string to `n` times `k` bits"""
+        """ Will hash string n times to k bits """
 
         assert k * n <= self.size, \
             f"{self.size} bit hash function {self.alg} cannot create {n} times {k} bits"
+
         self.k, self.n = k, n
 
     def bits(self, s):
-        """ Hashes string to byte array and wraps its bits as integer """
+        """ Hash string to byte array and return as integer """
 
         return int.from_bytes(self.alg(s.encode('utf-8')).digest(), byteorder='big', signed=False)
 
@@ -67,44 +68,45 @@ class Hasher:
         """Hashes string and slices results into bit fields returned as list of integers. """
 
         hash_bits = self.bits(s)
-        return [int((hash_bits >> int(i * self.k)) % (2 ** self.k)) for i in range(self.n)]
+        return [int((hash_bits >> int(i * self.k)) & (2 ** (self.k+1)) - 1) for i in range(self.n)]
+
 
 class BloomFilter:
     """https://en.wikipedia.org/wiki/Bloom_filter"""
 
     def __init__(self, m, k):
-        """m -- number of bits
-           k -- number of hash functions """
+        """New filter with m bits and k hash functions """
         assert is_power_of_2(m)
 
         self.m, self.k = m, k
         self.hasher = Hasher(int(math.log2(m)), k)
-        self.bits_as_int = 0
+        self.bits_as_int, self.added = 0, 0
 
     def add(self, str):
         # https://wiki.python.org/moin/BitwiseOperators
-        hashes = self.hasher.ints(str)
-        # print(f"Adding {hashes} to {self .inspect()}")
-        for h in hashes:
+        for h in self.hasher.ints(str):
             self.bits_as_int = self.bits_as_int | (2 ** h)
+            self.added += 1
 
-    def contains(self, str):
+    def query(self, str):
         for h in self.hasher.ints(str):
             if self.bits_as_int & (2 ** h) != 2 ** h:
                 return False
         return True
 
-    def fill_rate(self):
-        return (1.0 * self.bits_filled()) / self.m
-
-    def bits_filled(self):
-        return self.bit_mask().count("1")
+    def stats(self):
+        ones = self.bit_mask().count("1")
+        return {
+            '1s' : ones ,
+            'fill rate': ones / self.m,
+            'added': self.added
+        }
 
     def bit_mask(self):
         return bin(self.bits_as_int).lstrip('0b')
 
     def inspect(self):
-        return f"funcs={self.k}, bits={self.m}, fill={self.bits_filled()}, mask={self.bit_mask()}"
+        return f"funcs={self.k}, bits={self.m}, stats={self.stats()}, mask={self.bit_mask()}"
 
 class TestHashes(unittest.TestCase):
 
@@ -130,18 +132,18 @@ class TestBloom(unittest.TestCase):
     def test_single(self):
         bloom = BloomFilter(8, 64)
         s = random_string(5)
-        assert not bloom.contains(s)
+        assert not bloom.query(s)
         bloom.add(s)
-        assert bloom.contains(s)
+        assert bloom.query(s)
 
-    def test_many(self, n=100000):
+    def test_many(self, n=10000):
         strings = [random_string(5) for i in range(n)]
         print(f'Counting unique strings in: {strings} ')
-        bloom = bloom_insertions(2 ** 20, n)  # BloomFilter(2 ** 12, 8)
+        bloom = bloom_fp(0.2, n)  # BloomFilter(2 ** 12, 8)
 
         strings_approx = 0
         for s in strings:
-            if not bloom.contains(s):
+            if not bloom.query(s):
                 strings_approx += 1
                 bloom.add(s)
 
@@ -150,6 +152,13 @@ class TestBloom(unittest.TestCase):
         print(f'{strings_exact} unique strings in {len(strings)}. Approximated: {strings_approx}')
 
         assert 0 < strings_approx <= strings_exact
+
+        # Process finished with exit code 0
+        # Counting unique strings in: ['rkqeg', 'anqzw', ...
+        # epsilon=0.2, inserts=10000 --> bits=33497
+        # bits=65536, inserts=10000 --> hashes=5
+        # 9996 unique strings in 10000. Approximated: 9989
+        # Ran 1 test in 13.569s
 
 
 class TestRandom(unittest.TestCase):
